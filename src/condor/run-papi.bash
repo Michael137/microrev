2eccb33c5483fc2d11d41edadc422f444344f1bb
Michael Buch <mbuch@cs246.seas.harvard.edu>
2019-11-25 18:54:51 -0500

    condor files tweak

--- src/condor/run-papi.bash
+++ src/condor/run-papi.bash
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
-papi_native_avail
+ls /usr/local/lib/
 
-ls /usr/lib/*papi*
+sudo sysctl -w kernel.perf_event_paranoid=1
+/usr/local/bin/papi_avail -a
+/usr/local/bin/papi_native_avail
+
+grep NUMA=y /boot/config-`uname -r`
+# numactl --hardware
+
+./protocols.out
--- src/local/config.mk
+++ src/local/config.mk
@@ -1,7 +1,8 @@
 ROOT_DIR ?= .
 CXXFLAGS ?=
-CXX = clang++
+CXX = g++
 SHELL = /bin/bash
 UNAME := $(shell uname)
 PC_TYPE ?= WITH_PAPI_LL
-CXX_VERSION=c++17
+PAPI_DIR := /home/mbuch/papi/install
+CXX_VERSION ?= c++11
