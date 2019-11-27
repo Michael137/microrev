3b21e1dee3aeb38d70cb0ae398e0bf7c6402b9c2
Michael Buch <michaelbuch12@gmail.com>
2019-11-27 21:36:21 +0000

    rename file

--- src/local/src/cache.cpp
+++ src/local/src/cache.cpp
@@ -18,12 +18,17 @@
 
 using namespace pcnt;
 
-void __attribute__( ( optimize( "0" ) ) ) doloops( uint64_t stride )
+void __attribute__( ( optimize( "0" ) ) )
+doloops( uint64_t wset_sz, uint64_t stride = 4096, uint64_t repeat = 100000 )
 {
-	char arr[_4MB];
-	for( uint64_t i = 0; i < _4MB; i += stride )
-		for( uint64_t j = 0; j < stride; ++j )
-			arr[j + i] = ( j % 9 ) + 'A' - 1;
+	char c;
+	char arr[wset_sz];
+	for( uint64_t i = 0; i < wset_sz; i += stride )
+		arr[i] = ( i % 9 ) + 'A' - 1;
+
+	for( uint64_t i = 0; i < repeat; ++i )
+		for( uint64_t j = 0; j < wset_sz; j += stride )
+			c = arr[j];
 }
 
 void test_wset2() { doloops( _2KB ); }
@@ -36,6 +41,7 @@ void test_wset128() { doloops( _128KB ); }
 void test_wset256() { doloops( _256KB ); }
 void test_wset512() { doloops( _512KB ); }
 void test_wset1024() { doloops( _1MB ); }
+void test_wset2048() { doloops( _2MB ); }
 
 int main( int argc, char* argv[] )
 {
@@ -56,17 +62,22 @@ int main( int argc, char* argv[] )
 	CounterBenchmark<PAPILLCounter> cbench;
 	using Sched = Schedule<std::vector<std::string>>;
 
-	Sched core_1 = Sched{ 1,
-	                      std::function<decltype( test_wset32 )>{ test_wset32 },
-	                      { "PAPI_TOT_CYC", "PAPI_L2_DCA", "PAPI_L3_DCA" } };
+	Sched core_1
+	    = Sched{ 1,
+	             std::function<decltype( test_wset32 )>{ test_wset32 },
+	             { "perf::L1-DCACHE-LOAD-MISSES", "perf::L1-DCACHE-LOADS" } };
+	//"CYCLE_ACTIVITY:CYCLES_L1D_PENDING" } };
 	Sched core_2
 	    = Sched{ 2,
 	             std::function<decltype( test_wset32 )>{ test_wset256 },
-	             { "PAPI_TOT_CYC", "PAPI_L2_DCA", "PAPI_L3_DCA" } };
+	             { "perf::L1-DCACHE-LOAD-MISSES", "perf::L1-DCACHE-LOADS" } };
+	//"CYCLE_ACTIVITY:CYCLES_L1D_PENDING" } };
 	Sched core_3
 	    = Sched{ 3,
-	             std::function<decltype( test_wset32 )>{ test_wset1024 },
-	             { "PAPI_TOT_CYC", "PAPI_L2_DCA", "PAPI_L3_DCA" } };
+	             std::function<decltype( test_wset32 )>{ test_wset512 },
+	             { "perf::L1-DCACHE-LOAD-MISSES", "perf::L1-DCACHE-LOADS" } };
+	//"CYCLE_ACTIVITY:CYCLES_L1D_PENDING" } };
+
 	std::vector<Sched> vec{ core_1, core_2, core_3 };
 	cbench.counters_with_schedule<std::vector<std::string>>( vec );
 
