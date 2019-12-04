909d4064da75f3711a1e7e2161403dd8180b1416
Michael Buch <michaelbuch12@gmail.com>
2019-12-04 07:12:51 +0000

    add back config.mk

---
+++ src/local/config.mk
@@ -0,0 +1,8 @@
+ROOT_DIR ?= .
+CXXFLAGS ?=  -fsanitize=thread # -fsanitize=address
+CXX = g++
+SHELL = /bin/bash
+UNAME := $(shell uname)
+PC_TYPE ?= WITH_PAPI_LL
+PAPI_DIR := /home/gardei/papi/install
+CXX_VERSION ?= c++11
