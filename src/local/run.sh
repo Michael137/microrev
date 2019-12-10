6702e17e81ffdb7b49a08820b60d9308e3e64b5d
Michael Buch <michaelbuch12@gmail.com>
2019-12-10 22:33:13 +0000

    more results

--- src/local/run.sh
+++ src/local/run.sh
@@ -1,10 +1,6 @@
 rm -f dump.dat result.dat
-#LD_LIBRARY_PATH=/home/yeongilk/papi/install/lib ./bin/cacheline.out
-#LD_LIBRARY_PATH=/home/yeongilk/papi/install/lib ./bin/test/rd_latency.out
-#LD_LIBRARY_PATH=/home/yeongilk/papi/install/lib ./bin/test/a_latency.out
-#LD_LIBRARY_PATH=/home/yeongilk/papi/install/lib ./bin/test/c_latency.out
-LD_LIBRARY_PATH=/home/yeongilk/papi/install/lib ./bin/producer_consumer.out
-#LD_LIBRARY_PATH=/home/yeongilk/papi/install/lib ./bin/protocols.out
+LD_LIBRARY_PATH=/home/gardei/papi/install/lib ./bin/test/reader_writer_pingpong.out
+exit
 module load tools/python/3.6.5-anaconda-5.2.0
 python scripts/parser.py
 cat result.dat
