0e45d7a2c0f488845462f94b2cc5a2ee60ca3279
Michael Buch <mbuch@cs246.seas.harvard.edu>
2019-12-10 17:41:28 -0500

    optimized headers

--- src/local/run.sh
+++ src/local/run.sh
@@ -1,9 +1,5 @@
 rm -f dump.dat result.dat
-LD_LIBRARY_PATH=/home/gardei/papi/install/lib ./bin/test/reader_writer_pingpong.out
-exit
-module load tools/python/3.6.5-anaconda-5.2.0
-python scripts/parser.py
-cat result.dat
-#python scripts/parser.py
-#cat result.dat
-echo "Done"
+#./bin/test/a_latency.out
+#./bin/test/b_latency.out
+#./bin/test/c_latency.out
+./bin/producer_consumer.out
