88d2bda5ff39bdb49aed626bdd451b2b353ebfaf
zeronek501 <zeronek501@gmail.com>
2019-12-10 19:05:20 -0500

    counter print change

--- src/local/scripts/pro_con.py
+++ src/local/scripts/pro_con.py
@@ -2,6 +2,7 @@ import os
 import statistics
 import pandas as pd
 
+df=[0,0]
 test_res = {}
 
 lat_test_list = [
@@ -34,23 +35,36 @@ easy_tname = [
 easy_pname = ["G", "S", "L"]
 tdict = {value:lat_test_list[key] for (key, value) in enumerate(easy_tname)}
 pdict = {value:lat_place_list[key] for (key, value) in enumerate(easy_pname)}
+def read_dfs():
+    df[0] = pd.read_csv("result1.csv")
+    df[1] = pd.read_csv("result2.csv")
+    print(df[0])
+    print(df[1])
 
-def get_lat_abv(df, s):
-    return df[(df["Test"] == tdict[s[:-1]]) & (df["Placement"] == pdict[s[-1]])]["Cycles"].values[0]
 
-def calc_lat(df, s_list):
+def get_lat_abv(dfs, s):
+    if s[3] == "1":
+        df = dfs[0]
+    else:
+        df = dfs[1]
+    print(tdict, pdict)
+    print(s[:2], s[2])
+    print(tdict[s[:2]], pdict[s[2]])
+    return df[(df["Test"] == tdict[s[:2]]) & (df["Placement"] == pdict[s[2]])]["Cycles"].values[0]
+
+def calc_lat(df, s_list, cnt_list):
     lat_list = []
-    for s in s_list:
-        lat_list.append(get_lat_abv(df, s))
+    for i, s in enumerate(s_list):
+        lat_list.append(get_lat_abv(df, s) * cnt_list[i])
     return lat_list
 
 def get_ping_pong_lat(df, pl):
     trans_list = [
-            "SS", "SI", # Producer
-            "SS", "LM" # Consumer
+            "LML2", "LML1", "SS"+pl+"1", "SML1" , "SM"+pl+"3", "SM"+pl+"3",
+            "SM"+pl+"3", "SM"+pl+"3", "SML1", "SML2", "LML1", "LM"+pl+"3"
                 ]
-    s_list = [s + pl for s in trans_list]
-    lat_list = calc_lat(df, s_list)
+    cnt_list = [1,7,1,7,1,1,1,1,7,1,7,1]
+    lat_list = calc_lat(df, trans_list, cnt_list)
 
 def get_one_way_lat(df, pl):
     trans_list = [
@@ -59,3 +73,7 @@ def get_one_way_lat(df, pl):
                 ]
     s_list = [s + pl for s in trans_list]
     lat_list = calc_lat(df, s_list)
+
+if __name__ == "__main__":
+    read_dfs()
+    print(get_lat_abv(df, "LML1"))
